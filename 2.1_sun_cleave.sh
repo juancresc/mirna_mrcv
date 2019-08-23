ecbb525c6b72a2ad8e3d12c33d2d0f95c202e18e
Juan Manuel Crescente <you@example.com>
2019-08-23 11:54:54 -0300

    add api views

---
+++ 2.1_sun_cleave.sh
@@ -0,0 +1,31 @@
+#cat SRR1197125.trimmed.fastq | paste - - - - | sed 's/^@/>/g'| cut -f1-2 | tr '\t' '\n' > SRR1197125.fasta    
+
+set -x 
+path_cleaveland=/home/juan/Desktop/juan/bio/mirna_mite/sw/CleaveLand4
+path_deg=/home/juan/Desktop/juan/bio/mirna_mite/data/sun/degradome
+transcriptome=/home/juan/Desktop/juan/bio/mirna_mite/data/fixed.cdna.all.fa
+#RES
+path_mirna_fasta=/home/juan/Desktop/juan/bio/mirna_mrcv/data/mirna.deg.Y.fasta
+plots=/home/juan/Desktop/juan/bio/mirna_mrcv/data/sun_plots
+out_deg=/home/juan/Desktop/juan/bio/mirna_mrcv/data/sun_deg
+
+path_gsta=/home/juan/Desktop/juan/bio/mirna_mite/sw/CleaveLand4/GSTAr_v1-0
+export PATH=$PATH:$path_gsta
+path_bowtie=/home/juan/Desktop/juan/bio/sw/bowtie-1.2.2-linux-x86_64
+export PATH=$PATH:$path_bowtie
+path_samtools=/home/juan/Desktop/juan/bio/sw/samtools-0.1.19
+path_samtools=/home/juan/Desktop/juan/bio/sw/samtools-1.2
+export PATH=$PATH:$path_samtools
+
+file_name=SRR1197126
+mkdir $plots/${file_name}
+${path_cleaveland}/CleaveLand4.pl -e $path_deg/${file_name}.trimmed.21.fasta -u $path_mirna_fasta -n $transcriptome -t -o $plots/${file_name} > ${out_deg}/${file_name} 2> ${out_deg}/${file_name}.log
+
+#for file_name in SRR1197125  SRR1197126 SRR1197127  SRR1197128
+#for file_name in SRR1197125 SRR1197126 SRR1197127 SRR1197128
+#do
+#    mkdir $plots/${file_name}
+#    ${path_cleaveland}/CleaveLand4.pl -e $path_deg/${file_name}.trimmed.21.fasta -u $path_mirna_fasta -n $transcriptome -t -o $plots/${file_name} > ${out_deg}_${file_name} 2> ${out_deg}_${file_name}.log
+#done
+
+#${path_cleaveland}/CleaveLand4.pl -e $path_deg/SRR1197125.trimmed.fastq -u $path_mirna_fasta -n $transcriptome -t -o $plots > $out_deg
---
+++ README.md
@@ -0,0 +1,17 @@
+## counts.fixed.csv
+Archivo de recuento de reads sobre clusters sRNA (ShortStack)
+
+## counts.valid.csv
+counts.fixed.csv filtrado a los 135 miRNA válidos (1.1_xtract_ss_y.ipynb)
+
+## mirna.deg.csv
+11 miRNA validos acumulados diferencialmente (1.2_DEmiRNA.R)
+
+## mirna.deg.Y.fasta
+secuencias maduras de los 11 mirna.deg.csv (1.3_xtractDEGsmiRNA.ipynb)
+
+## mirna.res.deg.csv
+resultados de shorstack filtrados solo para los 11 miRNAs validos (1.3_xtractDEGsmiRNA.ipynb)
+
+## targets.fa
+Secuencias de todos los targets (2.2_cleave.ipynb)
